551415480f2ccff13d03e376ad872f7693c7336b
Stefaneva <stefaneva25@yahoo.com>
2018-07-29 20:33:03 +0300

    tz#2

--- src/main/java/stefan/licenta/homefinder/dto/EventDtoTransformer.java
+++ src/main/java/stefan/licenta/homefinder/dto/EventDtoTransformer.java
@@ -4,6 +4,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 import stefan.licenta.homefinder.entity.Event;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,13 +13,19 @@ import java.util.stream.Collectors;
 public class EventDtoTransformer {
 
     public EventDto transform(Event event){
+        Calendar calendarStart = Calendar.getInstance();
+        calendarStart.setTime(event.getStartDate());
+        calendarStart.add(Calendar.HOUR_OF_DAY, -3);
+        Calendar calendarEnd = Calendar.getInstance();
+        calendarEnd.setTime(event.getEndDate());
+        calendarEnd.add(Calendar.HOUR_OF_DAY, -3);
         EventDto eventDto = EventDto.builder()
                 .adId(event.getAd().getAdId())
                 .userEmail(event.getUser().getEmail())
                 .eventId(event.getEventId())
                 .status(event.getStatus())
-                .startDate(event.getStartDate().toString())
-                .endDate(event.getEndDate().toString())
+                .startDate(calendarStart.toString())
+                .endDate(calendarEnd.toString())
                 .message(event.getMessage())
                 .build();
         System.out.println(eventDto);
