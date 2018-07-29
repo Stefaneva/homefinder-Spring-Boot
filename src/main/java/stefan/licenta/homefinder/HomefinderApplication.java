7058ba756be8d66643c92ecb29131805e9e211ce
Stefaneva <stefaneva25@yahoo.com>
2018-07-29 19:30:15 +0300

    Timezone#4

--- src/main/java/stefan/licenta/homefinder/HomefinderApplication.java
+++ src/main/java/stefan/licenta/homefinder/HomefinderApplication.java
@@ -15,12 +15,7 @@ import java.util.TimeZone;
 @EnableJpaRepositories("stefan.licenta.homefinder")
 @EntityScan("stefan.licenta.homefinder.entity")
 public class HomefinderApplication {
-
-	@PostConstruct
-	void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Bucharest"));
-	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HomefinderApplication.class, args);
 	}
